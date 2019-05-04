cdff419e3eb516bae5b3d8e5accc76424cdd772d
Mark Rittman <mark.rittman@mjr-analytics.com>
2019-05-04 09:04:30 +0100

    commit of v1.0

--- ra_dw/models/customers/customer_master.sql
+++ ra_dw/models/customers/customer_master.sql
@@ -6,7 +6,7 @@ case when harvest_customer_id is not null then true else false end as is_service
                                             case when xero_is_supplier is true then true else false end as is_supplier_company,
 * from (select * from (
 SELECT customer_name, row_number() over (partition by lower(customer_name)) as c_r,
-hubspot_company_id, xero_contact_id, harvest_customer_id, harvest_address, xero_is_customer, xero_is_supplier, xero_customer_status, harvest_customer_created_at, harvest_customer_currency, harvest_customer_is_active, hubspot_first_deal_created_date, hubspot_twitterhandle, hubspot_country, hubspot_total_money_raised, hubspot_city, hubspot_total_revenue, hubspot_annual_revenue, hubspot_website, hubspot_owner_id, hubspot_industry, hubspot_linkedin_bio, hubspot_is_public, hubspot_domain, hubspot_created_date, hubspot_type, hubspot_state, hubspot_lifecycle_stage, hubspot_description FROM `ra-development.ra_data_warehouse.combined_raw_companies` 
+hubspot_company_id, xero_contact_id, harvest_customer_id, harvest_address, xero_is_customer, xero_is_supplier, xero_customer_status, harvest_customer_created_at, harvest_customer_currency, harvest_customer_is_active, hubspot_first_deal_created_date, hubspot_twitterhandle, hubspot_country, hubspot_total_money_raised, hubspot_city, hubspot_total_revenue, hubspot_annual_revenue, hubspot_website, hubspot_owner_id, hubspot_industry, hubspot_linkedin_bio, hubspot_is_public, hubspot_domain, hubspot_created_date, hubspot_type, hubspot_state, hubspot_lifecycle_stage, hubspot_description FROM {{ ref('combined_raw_companies') }} 
 group by 1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
 order by 1)
 where c_r = 1)
