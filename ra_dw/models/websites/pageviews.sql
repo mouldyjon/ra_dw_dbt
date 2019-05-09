a59674744c01c8306679807f2eba244657ab9b98
Mark Rittman <mark.rittman@mjr-analytics.com>
2019-05-09 07:48:39 +0100

    added geolookup for website data

--- ra_dw/models/websites/pageviews.sql
+++ ra_dw/models/websites/pageviews.sql
@@ -3,4 +3,23 @@
         materialized='table'
     )
 }}
-select * from {{ ref('segment_combined_pageviews')}}
+SELECT
+  *,
+  IFNULL(city, 'Other') AS city,
+  IFNULL(countryLabel, 'Other') AS countryLabel,
+  latitude,
+  longitude
+FROM (
+  SELECT
+    a.*,
+    NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip)) AS clientIpNum,
+    TRUNC(NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip))/(256*256)) AS classB
+  FROM
+    {{ ref('segment_combined_pageviews')}} ) AS a
+LEFT OUTER JOIN
+  {{ ref('geo_lookup')}} AS b
+ON
+  a.classB = b.classB
+  AND a.clientIpNum BETWEEN b.startIpNum AND b.endIpNum
+ORDER BY
+  id ASC
