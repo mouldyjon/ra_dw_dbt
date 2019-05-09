7c00b2781b949c86d7bac25300591881680d75ad
Mark Rittman <mark.rittman@mjr-analytics.com>
2019-05-09 08:03:07 +0100

    geo fix

--- ra_dw/models/websites/pageviews.sql
+++ ra_dw/models/websites/pageviews.sql
@@ -4,18 +4,16 @@
     )
 }}
 SELECT
-  *,
+  site, anonymous_id, ip, context_library_name, context_page_path, context_page_search, context_page_title, context_page_url, context_user_agent, id, _sdc_received_at, path, received_at, search, title, url, timestamp, referrer, country, region, postalCode, latitude, longitude, metroCode, areaCode,
   IFNULL(city, 'Other') AS city,
-  IFNULL(countryLabel, 'Other') AS countryLabel,
-  latitude,
-  longitude
+  IFNULL(countryLabel, 'Other') AS countryLabel
 FROM (
   SELECT
-    a.*,
+    site, anonymous_id, ip, context_library_name, context_page_path, context_page_search, context_page_title, context_page_url, context_user_agent, id, _sdc_received_at, path, received_at, search, title, url, timestamp, referrer,
     NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip)) AS clientIpNum,
     TRUNC(NET.IPV4_TO_INT64(NET.IP_FROM_STRING(ip))/(256*256)) AS classB
   FROM
-    {{ ref('segment_combined_pageviews')}} ) AS a
+    {{ ref('segment_combined_pageviews')}} p ) AS a
 LEFT OUTER JOIN
   {{ ref('geo_lookup')}} AS b
 ON
