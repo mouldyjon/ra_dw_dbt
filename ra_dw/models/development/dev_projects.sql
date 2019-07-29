e6668626f24555a980fe8b00370ba6ef628ffe8f
Mark Rittman <mark.rittman@rittmananalytics.com>
2019-07-29 22:21:23 +0100

    Added Jira stories (issues) and projects

---
+++ ra_dw/models/development/dev_projects.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+SELECT
+  *
+FROM (
+  SELECT
+    id,
+    lead.displayname,
+    lead.active,
+    name,
+    projectkeys,
+    projecttypekey,
+    _sdc_sequence,
+    MAX(_sdc_sequence) OVER (PARTITION BY id ORDER BY _sdc_sequence RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS max_sdc_sequence,
+    description
+  FROM
+    {{ ref('jira_projects') }})
+WHERE
+  _sdc_sequence = max_sdc_sequence
