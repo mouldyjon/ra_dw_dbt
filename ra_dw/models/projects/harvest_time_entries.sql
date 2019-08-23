da2b75d4be65c196cf12a7efadcfa0ec73f9e811
Mark Rittman <mark.rittman@rittmananalytics.com>
2019-08-23 23:35:56 +0100

    fix in harvest_time_entries

--- ra_dw/models/projects/harvest_time_entries.sql
+++ ra_dw/models/projects/harvest_time_entries.sql
@@ -8,11 +8,11 @@ SELECT
 FROM (
     SELECT
         *,
-        MAX(_sdc_sequence) OVER (PARTITION BY id ORDER BY _sdc_sequence RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_sdc_sequence
+        MAX(t._sdc_sequence) OVER (PARTITION BY id ORDER BY t._sdc_sequence RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_sdc_sequence
     FROM
         {{ ref('harvest_base_time_entries') }}
     ) t
 JOIN {{ ref('harvest_users') }} u
     ON t.user_id = u.id
 WHERE
-    _sdc_sequence = latest_sdc_sequence
+    t._sdc_sequence = t.latest_sdc_sequence
