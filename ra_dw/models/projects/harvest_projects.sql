a0037c783b85a6e51b7c4c655e67b8dff06814f2
Mark Rittman <mark.rittman@rittmananalytics.com>
2019-08-25 15:01:50 +0100

    added harvest projects

---
+++ ra_dw/models/projects/harvest_projects.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+SELECT
+  *
+FROM (
+  SELECT
+    *,
+    MAX(updated_at) OVER (PARTITION BY id ORDER BY updated_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_updated_at
+  FROM
+    {{ ref('harvest_projects') }}
+  ORDER BY
+    id,
+    updated_at)
+WHERE
+  updated_at = latest_updated_at
