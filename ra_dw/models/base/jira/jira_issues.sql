9687ad53cd0aec0fc64358f46c5e09b680dbcc70
Mark Rittman <mark.rittman@rittmananalytics.com>
2019-12-29 23:14:17 +0000

    fixes

--- ra_dw/models/base/jira/jira_issues.sql
+++ ra_dw/models/base/jira/jira_issues.sql
@@ -32,6 +32,20 @@ FROM (
     fields.lastviewed,
     fields.updated,
     timestamp(substr(fields.statuscategorychangedate,1,23)) status_change_ts,
+    case when fields.status.statuscategory.name = 'Done' then 1 else 0 end as count_completed,
+    case when fields.status.statuscategory.name = 'In Progress' then 1 else 0 end as count_in_progress,
+    case when fields.status.statuscategory.name = 'To Do' then 1 else 0 end as count_unassigned,
+    case when fields.issuetype.name = 'Task' then 1 else 0 end as count_tasks,
+    case when fields.issuetype.name = 'Sub-task' then 1 else 0 end as count_subtasks,
+    case when fields.issuetype.name = 'Bug' then 1 else 0 end as count_bugs,
+    case when fields.issuetype.name = 'Support' then 1 else 0 end as count_support_tickets,
+    case when fields.project.projecttypekey = 'software' then 'Jira' else 'Jira Service Desk' end as service_category,
+    case when fields.project.name like '%Florence%' then 'Florence'
+         when fields.project.name like '%11:FS%' then '11:FS'
+         when upper(fields.project.name) like '%INTO%' then 'INTO University Partnerships'
+         else fields.project.name end as project_grouping,
+    case when fields.status.statuscategory.name = 'Done' then true else false end as issue_completed,
+    case when fields.status.statuscategory.name = 'Done' then timestamp_diff(timestamp(substr(fields.statuscategorychangedate,1,23)),fields.created,HOUR) end as issue_hours_to_complete,
     _sdc_batched_at,
     MAX(_sdc_batched_at) OVER (PARTITION BY id ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS max_sdc_batched_at
   FROM
