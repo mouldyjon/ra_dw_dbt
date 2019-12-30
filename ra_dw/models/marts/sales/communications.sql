fab669c0d2d2a02c76b673871c70b852fe3cea8a
Mark Rittman <mark.rittman@rittmananalytics.com>
2019-12-30 12:43:46 +0000

    applied fix to communications

--- ra_dw/models/marts/sales/communications.sql
+++ ra_dw/models/marts/sales/communications.sql
@@ -5,71 +5,54 @@
 }}
 
 
-with engagements as (
-
-    select * from {{ ref('hubspot_engagements') }}
-
-),
-
-
-owners as (
-
-    select * from {{ ref('hubspot_owners') }}
-
-),
-
-communications_id_with_max as (
-
-    select
+WITH
+  deduped_communications AS (
+  SELECT
+    *
+  FROM (
+    SELECT
       *,
-      max(_sdc_batched_at) over (partition by engagement.id order by _sdc_batched_at range between unbounded preceding and unbounded following) as latest_sdc_batched_at
-
-    from engagements
-
-),
-
-latest_version as (
-
-    select * from communications_id_with_max
-    where _sdc_batched_at = latest_sdc_batched_at
-
-),
-
-communications as (
-
-    select
-
-      engagement.id as engagement_id,
-      engagement_id as communications_id,
-      associations.companyids[offset(off)] as hubspot_company_id,
-      associations.dealids as sales_opportunity_id,
-      engagement.timestamp as communication_timestamp,
-      engagement.ownerid as ownerid,
-      engagement.type as communication_type,
-      metadatato.email as communications_to_email,
-      metadata.text as communications_text,
-      metadata.subject as communications_subject,
-      concat(concat(metadata.from.firstname,' '), metadata.from.lastname) as communications_from_firstname_lastname,
-      metadata.status as communications_status,
-      metadatacc.email as communications_cc_email
-
-    from
-
-      latest_version,
-      unnest(associations.companyids) with offset off,
-      unnest(associations.dealids),
-      unnest(metadata.cc) metadatacc,
-      unnest(metadata.to) metadatato
-
-),
-
-
-final as (
-
-    select * from communications
-
-    left outer join owners using (ownerid)
-
-)
-
-select * from final
+      MAX(_sdc_batched_at) OVER (PARTITION BY engagement_id ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_sdc_batched_at
+    FROM
+      {{ ref('hubspot_engagements') }} )
+  WHERE
+    _sdc_batched_at = latest_sdc_batched_at),
+  owners AS (
+  SELECT
+    *
+  FROM (
+    SELECT
+      *,
+      MAX(_sdc_batched_at) OVER (PARTITION BY ownerid ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_sdc_batched_at
+    FROM
+      {{ ref('hubspot_owners') }} )
+  WHERE
+    _sdc_batched_at = latest_sdc_batched_at)
+SELECT
+  engagement.id AS engagement_id,
+  engagement_id AS communications_id,
+  associations.companyids[OFFSET(off)] AS hubspot_company_id,
+  associations.dealids AS sales_opportunity_id,
+  engagement.timestamp AS communication_timestamp,
+  engagement.ownerid AS ownerid,
+  engagement.type AS communication_type,
+  metadata.text AS communications_text,
+  metadata.subject AS communications_subject,
+  CONCAT(CONCAT(metadata.FROM.firstname,' '), metadata.FROM.lastname) AS communications_from_firstname_lastname,
+  metadata.status AS communications_status,
+  metadatato.email AS communications_to_email,
+  concat(cast(engagement_id as string),coalesce(cast(associations.companyids[OFFSET(off)] as string),'')) as pk
+FROM (
+  SELECT
+    deduped_communications.*
+  FROM
+    deduped_communications
+  LEFT OUTER JOIN
+    owners
+  ON
+    deduped_communications.engagement.ownerid = owners.ownerid) communications,
+  UNNEST(communications.associations.companyids) WITH OFFSET off
+LEFT JOIN
+  communications.associations.dealids
+LEFT JOIN
+  communications.metadata.TO metadatato
