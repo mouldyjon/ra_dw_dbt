fbc55729729c73ba816e73b604d074b69fa8ad4e
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-01-11 11:43:57 +0000

    more deals fixes

--- ra_dw/models/marts/sales/deals.sql
+++ ra_dw/models/marts/sales/deals.sql
@@ -4,90 +4,109 @@
     )
 }}
 
-
-
 with deals as (
 
-        select *  from (
-          select *,
-          MAX(_sdc_batched_at) OVER (PARTITION BY dealid ORDER BY _sdc_batched_at RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) latest_sdc_batched_at
-           from {{ source('hubspot', 'deals') }})
-            where latest_sdc_batched_at = _sdc_batched_at
-
+    select * from {{ ref('deals_snapshot') }}
 
 ),
 
-new_deal as (
+stages as (
+
+    select * from {{ ref('pipeline_stages') }}
+
+),
+
+pipelines as (
+
+    select * from {{ ref('pipelines') }}
+
+),
+
+joining as (
+
+    select *,
+    timestamp_diff(current_timestamp,d.start_date_ts,DAY) as days_until_end,
+    timestamp(date_add(date(d.start_date_ts), interval safe_cast(d.duration_days as int64) day)) as end_date_ts,
+    case when (s.stage_label in ('Closed Won and Scheduled','Verbally Won and Working at Risk')
+    and timestamp_diff(current_timestamp,start_date_ts,DAY) < 365/2)
+    or
+      (s.stage_label in ('Closed Won and Scheduled','Verbally Won and Working at Risk')
+       and
+       d.start_date_ts < current_timestamp
+       and date_add(date(d.start_date_ts), interval safe_cast(d.duration_days as int64) day) > current_date)
+      then true else false end as is_active
+
+     from deals d
+
+    left join stages s on d.dealstage = s.stageid
+
+    left join pipelines p on s.pipelineid = p.pipelineid
+
+    left outer join {{ ref('owners') }} u
+
+    on safe_cast(d.hubspot_owner_id as int64) = u.ownerid
+
+),
+
+scd_deals as (
 
     select
-      properties.hs_sales_email_last_replied.value AS sales_email_last_replied,
-      properties.closed_lost_reason.value AS closed_lost_reason,
-      properties.dealname.value AS dealname,
-      properties.hubspot_owner_id.value AS hubspot_owner_id,
-      properties.hubspot_owner_id.sourceid as hubspot_owner_email,
-      properties.hs_lastmodifieddate.value AS lastmodifieddate,
-      properties.notes_last_updated.value AS notes_last_updated,
-      properties.dealstage.value AS dealstage,
-      properties.dealstage.value as dealstage_id,
-      properties.dealstage.timestamp as dealstage_ts,
-      properties.pipeline.value AS pipeline,
-      properties.closedate.value AS closedate,
-      properties.createdate.value AS createdate,
-      properties.amount_in_home_currency.value AS amount,
-      properties.notes_last_contacted.value AS notes_last_contacted,
-      properties.amount_in_home_currency.value AS amount_in_home_currency,
-      properties.hubspot_owner_assigneddate.value AS hubspot_owner_assigneddate,
-      properties.num_notes.value AS num_notes,
-      properties.description.value AS description,
-      properties.dealstage.source as source, -- added 06/11/1
-      properties.dealstage.sourceid as salesperson_email, -- added 06/11/2019
-      case when properties.closedate.value is not null then true else false end AS is_closed, -- added 18/12/2019
-      properties.pricing_model.value AS pricing_model, -- added 18/12/2019
-      properties.source.value as deal_source,
-      properties.products_in_solution.value as products_in_solution,
-      properties.sprint_type.value as sprint_type,
-      properties.days_to_close.value as days_to_close,
-      properties.partner_referral.value as partner_referral_type,
-      properties.deal_components.value as deal_components,
-      properties.dealtype.value as deal_type,
-      properties.assigned_consultant.value as assigned_consultant,
-      case when timestamp_millis(safe_cast(properties.delivery_schedule_date.value as int64)) > timestamp_millis(safe_cast(properties.delivery_start_date.value as int64))  then timestamp_millis(safe_cast(properties.delivery_schedule_date.value as int64))
-            when timestamp_millis(safe_cast(properties.delivery_start_date.value as int64)) >= timestamp_millis(safe_cast(properties.delivery_schedule_date.value as int64)) then timestamp_millis(safe_cast(properties.delivery_start_date.value as int64))
-            when timestamp_millis(safe_cast(properties.delivery_start_date.value as int64)) is null then timestamp_millis(safe_cast(properties.delivery_schedule_date.value as int64))
-            when timestamp_millis(safe_cast(properties.delivery_schedule_date.value as int64)) is null then timestamp_millis(safe_cast(properties.delivery_start_date.value as int64))
-            end as start_date_ts,
-      case when properties.number_of_sprints.value is not null then properties.number_of_sprints.value * 14 end as duration_days,
-      case when properties.deal_components.value like '%Services%' then 1 else 0 end as count_services_deal_component,
-      case when properties.deal_components.value like '%Training%' then 1 else 0 end as count_support_deal_component,
-      case when properties.deal_components.value like '%License Referral%' then 1 else 0 end as count_license_referral_deal_component,
-      case when properties.deal_components.value like '%Managed Services%' then 1 else 0 end as count_managed_services_deal_component,
-      case when properties.sprint_type.value like '%Data Analytics%' then 1 else 0 end as count_data_analytics_sprint_type,
-      case when properties.sprint_type.value like '%Data Engineering%' then 1 else 0 end as count_data_engineering_sprint_type,
-      case when properties.sprint_type.value like '%Data Science%' then 1 else 0 end as count_data_science_sprint_type,
-      case when properties.sprint_type.value like '%Data Strategy%' then 1 else 0 end as count_data_strategy_sprint_type,
-      case when properties.sprint_type.value like '%Data Integration%' then 1 else 0 end as count_data_integration_sprint_type,
-      case when properties.sprint_type.value like '%Data Collection%' then 1 else 0 end as count_data_collection_sprint_type,
-      case when properties.products_in_solution.value like '%Looker%' then 1 else 0 end as count_looker_product_in_solution,
-      case when properties.products_in_solution.value like '%dbt%' then 1 else 0 end as count_dbt_product_in_solution,
-      case when properties.products_in_solution.value like '%Stitch%' then 1 else 0 end as count_stitch_product_in_solution,
-      case when properties.products_in_solution.value like '%Segment%' then 1 else 0 end as count_segment_product_in_solution,
-      case when properties.products_in_solution.value like '%GCP%' then 1 else 0 end as count_gcp_product_in_solution,
-      case when properties.products_in_solution.value like '%Snowflake%' then 1 else 0 end as count_snowflake_product_in_solution,
-      case when properties.products_in_solution.value like '%Fivetran%' then 1 else 0 end as count_fivetran_product_in_solution,
-      case when properties.products_in_solution.value like '%Qubit%' then 1 else 0 end as count_qubit_product_in_solution,
-      case when properties.source.value like '%Partner Referral%' then 1 else 0 end as count_partner_referral_source,
-      case when properties.source.value like '%CEO Network%' then 1 else 0 end as count_ceo_network_source,
-      case when properties.source.value like '%Staff Referral%' then 1 else 0 end as count_staff_referral_referral_source,
-      case when properties.source.value like '%Organic Search%' then 1 else 0 end as count_organic_search_source,
-      case when properties.source.value like '%Repeat Customer%' then 1 else 0 end as count_repeat_customer_source,
-      case when properties.source.value like '%Paid Search/Campaign%' then 1 else 0 end as count_paid_search_source,
-      associations.associatedcompanyids[offset(off)] as associatedcompanyids, -- added 18/12/2019
-      dealid AS deal_id,
-      _sdc_batched_at
 
-    from deals,
-              unnest(associations.associatedcompanyids) with offset off
+-- SCD2 "historical values" attributes
+
+    *,
+
+-- SCD3 "previous value" attributes
+
+
+      lag(dealstage_ts) over (partition by deal_id order by dbt_updated_at) previous_dealstage_ts,
+      lag(stage_displayorder) over (partition by deal_id order by dbt_updated_at) previous_stage_displayorder,
+      lag(stage_label) over (partition by deal_id order by dbt_updated_at) previous_stage_label,
+
+
+-- SCD1 "current" value attributes
+
+      last_value(dealstage_ts) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_dealstage_ts,
+      last_value(dealname) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_dealname,
+      last_value(createdate) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_createdate,
+      last_value(hubspot_owner_id) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_hubspot_owner_id,
+      last_value(hubspot_owner_email) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_hubspot_owner_email,
+      last_value(dealstage_id) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_dealstage_id,
+      last_value(amount) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_amount,
+      last_value(probability) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_probability,
+      last_value(closedate) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_closedate,
+      last_value(stage_displayorder) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_stage_displayorder,
+      last_value(salesperson_full_name) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_salesperson_full_name,
+      last_value(salesperson_email) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_salesperson_email,
+      last_value(stage_label) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_stage_label,
+      last_value(is_active) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_is_active,
+      last_value(start_date_ts) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_start_date_ts,
+      last_value(duration_days) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_duration_days,
+      last_value(end_date_ts) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) current_end_date_ts,
+
+
+
+
+
+
+-- and "original" value attributes
+
+      first_value(createdate) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) original_createddate,
+      first_value(amount) over (partition by deal_id order by dbt_updated_at ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) original_amount
+
+    from joining
+
+),
+
+date_difference as (
+
+    select *,
+      timestamp_diff(dealstage_ts,previous_dealstage_ts, day) as days_between_stage,
+      timestamp_diff(current_timestamp(),original_createddate,day) as days_since_deal_created,
+      current_amount - original_amount as amount_diff_since_deal_created
+
+    from scd_deals
 
 )
 
-select * from new_deal
+select * except (pipelineid,_sdc_batched_at) from date_difference
