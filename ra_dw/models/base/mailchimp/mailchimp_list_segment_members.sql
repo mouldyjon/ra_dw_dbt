4010eb9b156ddcfc5894ed3f9b57844786112183
RobBramwell <59574904+RobBramwell@users.noreply.github.com>
2020-01-25 15:07:17 +0000

    added name changes to staging models

--- ra_dw/models/base/mailchimp/mailchimp_list_segment_members.sql
+++ ra_dw/models/base/mailchimp/mailchimp_list_segment_members.sql
@@ -1,1 +1,31 @@
-select * from {{ source('stitch_mailchimp', 'list_segment_members') }}
+SELECT
+  _sdc_batched_at AS _sdc_batched_at,
+  _sdc_received_at AS _sdc_received_at,
+  _sdc_sequence AS _sdc_sequence,
+  _sdc_table_version AS _sdc_table_version,
+  email_address AS email_address,
+  id AS list_segment_id,
+  ip_opt AS ip_opted_in,
+  language AS language,
+  last_changed AS last_changed,
+  list_id AS list_id,
+  location.country_code AS country_code,
+  location.latitude AS latitude,
+  location.longitude AS longitude,
+  location.timezone AS timezone,
+  member_rating AS member_rating,
+  merge_fields.address AS address,
+  merge_fields.birthday AS birthday,
+  merge_fields.fname AS fname,
+  merge_fields.lname AS Forename,
+  merge_fields.phone AS phone,
+  stats.avg_click_rate AS avg_click_rate,
+  stats.avg_open_rate AS avg_open_rate,
+  status AS status,
+  timestamp_opt AS opted_in_at,
+  unique_email_id AS email_id,
+FROM
+  {{ source(
+    'stitch_mailchimp',
+    'list_segment_members'
+  ) }}
--- ra_dw/models/base/mailchimp/mailchimp_reports_email_activity.sql
+++ ra_dw/models/base/mailchimp/mailchimp_reports_email_activity.sql
@@ -1,1 +1,19 @@
-select * from {{ source('stitch_mailchimp', 'reports_email_activity') }}
+SELECT
+  _sdc_received_at AS _sdc_received_at,
+  _sdc_sequence AS _sdc_sequence,
+  _sdc_table_version AS _sdc_table_version,
+  action AS Event,
+  campaign_id AS campaign_id,
+  email_address AS email_address,
+  email_id AS email_id,
+  ip AS ip,
+  list_id AS list_id,
+  list_is_active AS list_is_active,
+  TIMESTAMP AS event_at,
+  TYPE AS bounce_type,
+  url AS url,
+FROM
+  {{ source(
+    'stitch_mailchimp',
+    'reports_email_activity'
+  ) }}
