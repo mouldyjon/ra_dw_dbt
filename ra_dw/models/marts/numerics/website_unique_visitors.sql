b1f010eaa287cc4e9fe84e10f4bc3aeb7ad6c2d1
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-02-22 22:14:34 +0000

    fix

--- ra_dw/models/marts/numerics/website_unique_visitors.sql
+++ ra_dw/models/marts/numerics/website_unique_visitors.sql
@@ -3,7 +3,7 @@
         materialized='table'
     )
 }}
-SELECT FORMAT_TIMESTAMP('%Y/%W',timestamp_trunc(session_start_tstamp,WEEK)) as week, count(distinct blended_user_id)
+SELECT FORMAT_TIMESTAMP('%Y/%W',timestamp_trunc(session_start_tstamp,WEEK)) as week, count(distinct blended_user_id) as weekly_unique_visitors
 FROM `ra-development.analytics.segment_web_sessions__stitched`
 where date(timestamp_trunc(session_start_tstamp,WEEK)) >= date_sub(current_date,interval 6 week)
 and first_page_url_host = 'rittmananalytics.com'
