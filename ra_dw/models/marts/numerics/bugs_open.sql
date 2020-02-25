08effa056a5e5d2be35a97836476c62ff7cf1608
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-02-25 13:11:56 +0000

    added refs to numerics models

--- ra_dw/models/marts/numerics/bugs_open.sql
+++ ra_dw/models/marts/numerics/bugs_open.sql
@@ -3,10 +3,10 @@
         materialized='table'
     )
 }}
-WITH tickets AS (SELECT * from `ra-development.ra_data_warehouse_dbt_prod.tickets`
+WITH tickets AS (SELECT * from {{ ref('tickets') }}
       )
 SELECT
     COUNT(tickets.id ) AS tickets_total
-FROM `ra-development.analytics.customer_master` AS customer_master
-    LEFT JOIN `ra-development.analytics.tickets` AS tickets ON customer_master.customer_id = tickets.customer_id
+FROM {{ ref('customer_master') }} AS customer_master
+    LEFT JOIN tickets AS tickets ON customer_master.customer_id = tickets.customer_id
 WHERE (tickets.issuetype_name ) = 'Bug'AND (NOT COALESCE(tickets.issue_completed  , FALSE))
--- ra_dw/models/marts/numerics/channels.sql
+++ ra_dw/models/marts/numerics/channels.sql
@@ -7,9 +7,9 @@
 SELECT
     coalesce(deals.current_partner_referral_type,'Direct')  AS channel,
     ROUND(COALESCE(CAST( ( SUM(DISTINCT (CAST(ROUND(COALESCE(( cast(deals.current_amount * (deals.current_probability/1) as int64))   ,0)*(1/1000*1.0), 9) AS NUMERIC) + (cast(cast(concat('0x', substr(to_hex(md5(CAST(( deals.deal_id )    AS STRING))), 1, 15)) as int64) as numeric) * 4294967296 + cast(cast(concat('0x', substr(to_hex(md5(CAST(( deals.deal_id )    AS STRING))), 16, 8)) as int64) as numeric)) * 0.000000001 )) - SUM(DISTINCT (cast(cast(concat('0x', substr(to_hex(md5(CAST(( deals.deal_id )    AS STRING))), 1, 15)) as int64) as numeric) * 4294967296 + cast(cast(concat('0x', substr(to_hex(md5(CAST(( deals.deal_id )    AS STRING))), 16, 8)) as int64) as numeric)) * 0.000000001) )  / (1/1000*1.0) AS FLOAT64), 0), 6) AS weighted_amount
-FROM `ra-development.analytics.customer_master` AS customer_master
-    LEFT JOIN `ra-development.analytics.bridge_associatedcompanyids` AS bridge ON customer_master.hubspot_company_id = bridge.associatedcompanyids
-    INNER JOIN `ra-development.analytics.deals` AS deals ON bridge.deal_id = deals.deal_id
+FROM {{ ref('customer_master') }} AS customer_master
+    LEFT JOIN {{ ref('bridge_associatedcompanyids') }} AS bridge ON customer_master.hubspot_company_id = bridge.associatedcompanyids
+    INNER JOIN {{ ref('deals') }} AS deals ON bridge.deal_id = deals.deal_id
 WHERE (deals.current_stage_label ) IN ('Closed Won and Scheduled', 'Verbally Won and Working at Risk', 'Deal Agreed & Awaiting Sign-off', 'Proposal Sent', 'Presentation Given & Sprints Scoped', 'Initial Enquiry', 'Meeting and Sales Qualified')
 GROUP BY
     1
