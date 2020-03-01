01888ef28b9896b1c88dabc17395c59f67818bb5
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-03-01 23:20:39 +0000

    more numerics

--- ra_dw/models/marts/numerics/support_tickets_open.sql
+++ ra_dw/models/marts/numerics/support_tickets_open.sql
@@ -3,13 +3,13 @@
         materialized='table'
     )
 }}
-WITH tickets AS (SELECT * from `ra-development.ra_data_warehouse_dbt_prod.tickets`
+WITH tickets AS (SELECT * from {{ ref('tickets') }}
       )
 SELECT
     tickets.displayname  AS tickets_displayname,
     COUNT(tickets.id ) AS tickets_total
 FROM `ra-development.analytics.customer_master` AS customer_master
-    LEFT JOIN `ra-development.analytics.tickets` AS tickets ON customer_master.customer_id = tickets.customer_id
+    LEFT JOIN tickets AS tickets ON customer_master.customer_id = tickets.customer_id
 WHERE (tickets.issuetype_name ) = 'Support'AND (NOT COALESCE(tickets.issue_completed  , FALSE))
 GROUP BY
     1
