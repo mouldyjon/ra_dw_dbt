a9f0542c9247bbc8dd8c0a1a254338ae8764361b
Mark Rittman <mark.rittman@rittmananalytics.com>
2020-03-06 21:01:06 +0000

    Revert "segment tidyup"
    This reverts commit d1cdbe73e6c5e6a2090d30ff52e312afbb15014c.

---
+++ ra_dw/macros/cross-adapter-modeling/sessionization/segment_web_sessions__stitched.sql
@@ -0,0 +1,57 @@
+{% macro segment_web_sessions__stitched() %}
+
+    {{ adapter_macro('segment_web_sessions__stitched') }}
+
+{% endmacro %}
+
+
+{% macro default__segment_web_sessions__stitched() %}
+
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'session_id',
+    sort = 'session_start_tstamp',
+    dist = 'session_id'
+    )}}
+
+with sessions as (
+
+    select * from {{ref('segment_web_sessions__initial')}}
+
+    {% if is_incremental() %}
+        where session_start_tstamp > (
+          select {{
+            dbt_utils.safe_cast(
+              dbt_utils.dateadd(
+                'hour',
+                -var('segment_sessionization_trailing_window'),
+                'max(session_start_tstamp)'),
+              'timestamp') }}
+          from {{ this }})
+    {% endif %}
+
+),
+
+id_stitching as (
+
+    select * from {{ref('segment_web_user_stitching')}}
+
+),
+
+joined as (
+
+    select
+
+        sessions.*,
+
+        coalesce(id_stitching.user_id, sessions.anonymous_id)
+            as blended_user_id
+
+    from sessions
+    left join id_stitching using (anonymous_id)
+
+)
+
+select * from joined
+
+{% endmacro %}
